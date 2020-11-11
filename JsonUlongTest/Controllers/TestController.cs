e72793f5d9161c966572171b5b4bd01b127de53d
Galen Guyer <galen@galenguyer.com>
2020-11-10 22:13:04 -0500

    add demo

---
+++ JsonUlongTest/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JsonUlongTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(new
+            {
+                ulong_val = 351231282157453322,
+                string_val = 351231282157453322.ToString()
+            });
+        }            
+    }
+}
